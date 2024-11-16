d9bab7f73bf5724557050b9ffd30305761214b77
Nick Ponomar <nick@bytefusionlabs.com>
2024-11-16 22:10:28 +0100

    Update socket.ts

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 const isBrowser = typeof window !== "undefined";
-const host = "http://localhost:3000";
+const host = "wss://localhost";
 
 export const socketClient = isBrowser ? io(host) : {};
