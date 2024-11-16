4d560312d6bf2c36c0c3831a6ef040e3ddcf9dc8
Nick Ponomar <nick@bytefusionlabs.com>
2024-11-16 21:03:45 +0100

    Socket config test

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 const isBrowser = typeof window !== "undefined";
-const host = "http://localhost:3000";
+const host = "https://localhost:3000";
 
 export const socketClient = isBrowser ? io(host) : {};
