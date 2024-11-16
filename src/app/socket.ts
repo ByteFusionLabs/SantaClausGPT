6d6957647b89a474665aa6580d023b6126e757db
Nick Ponomar <nick@bytefusionlabs.com>
2024-11-16 21:14:02 +0100

    Update socket.ts

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 const isBrowser = typeof window !== "undefined";
-const host = "https://santaclausgpt-production.up.railway.app:3000";
+const host = "http://localhost:3000";
 
 export const socketClient = isBrowser ? io(host) : {};
