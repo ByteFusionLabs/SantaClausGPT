b91becfb8a63c9e875ae9b11b7dfa78178668c42
Nick Ponomar <nick@bytefusionlabs.com>
2024-11-16 22:17:27 +0100

    RAILWAY_PUBLIC_DOMAIN env variable

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -2,7 +2,9 @@ import { io, Socket } from "socket.io-client";
 
 const isBrowser = typeof window !== "undefined";
 const host = isBrowser
-    ? process.env.NEXT_PUBLIC_SOCKET_HOST || "wss://santaclausgpt-production.up.railway.app" // Replace with your Railway app URL
-    : ""; // No socket connection on the server-side
+    ? (process.env.RAILWAY_PUBLIC_DOMAIN
+        ? `wss://${process.env.RAILWAY_PUBLIC_DOMAIN}`
+        : "ws://localhost:3000") // Fallback to local for development
+    : ""; // No socket connection on server-side
 
 export const socketClient: Socket | null = isBrowser ? io(host, { transports: ['websocket'] }) : null;
