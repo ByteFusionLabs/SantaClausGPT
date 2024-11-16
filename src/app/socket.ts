a7f555b2ac8ada288be9069be98803e927914efb
Nick Ponomar <nick@bytefusionlabs.com>
2024-11-16 22:21:28 +0100

    Socket host url handling (#1)

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,6 +1,10 @@
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 const isBrowser = typeof window !== "undefined";
-const host = "http://localhost:3000";
+const host = isBrowser
+    ? (process.env.RAILWAY_PUBLIC_DOMAIN
+        ? `wss://${process.env.RAILWAY_PUBLIC_DOMAIN}`
+        : "ws://localhost:3000") // Fallback to local for development
+    : ""; // No socket connection on server-side
 
-export const socketClient = isBrowser ? io(host) : {};
+export const socketClient: Socket | null = isBrowser ? io(host, { transports: ['websocket'] }) : null;
