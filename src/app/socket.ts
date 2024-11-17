237d4237223a7bd9fd639104e4eafff4384fde79
Nick Ponomar <nick@bytefusionlabs.com>
2024-11-17 15:46:58 +0100

    Update socket.ts

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -4,11 +4,11 @@ const isBrowser = typeof window !== "undefined";
 const host = isBrowser
     ? (process.env.RAILWAY_PUBLIC_DOMAIN
         ? `wss://${process.env.RAILWAY_PUBLIC_DOMAIN}`
-        : "ws://localhost:3000") // Fallback to local for development
-    : ""; // No socket connection on server-side
+        : "ws://localhost:3000") // Use ws:// for local, wss:// for production
+    : "";
 
 console.info("isBrowser: ", isBrowser);
 console.info("Host: ", host);
-console.info("Environment Variables: ", process.env);
+console.info("process.env.RAILWAY_PUBLIC_DOMAIN: ", process.env.RAILWAY_PUBLIC_DOMAIN);
 
 export const socketClient: Socket | null = isBrowser ? io(host, { transports: ['websocket'] }) : null;
