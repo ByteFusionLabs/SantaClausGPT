caeff925c079be9512df6dc2a24e5ad97d18938d
Nick Ponomar <nick@bytefusionlabs.com>
2024-11-17 15:57:38 +0100

    Update socket.ts

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -7,8 +7,4 @@ const host = isBrowser
         : "ws://localhost:3000") // Use ws:// for local, wss:// for production
     : "";
 
-console.info("isBrowser: ", isBrowser);
-console.info("Host: ", host);
-console.info("process.env.NEXT_PUBLIC_WEBSOCKET_DOMAIN: ", process.env.NEXT_PUBLIC_WEBSOCKET_DOMAIN);
-
 export const socketClient: Socket | null = isBrowser ? io(host, { transports: ['websocket'] }) : null;
