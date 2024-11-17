6c2dcf6e12a988d00c6399f304606333351787ce
Nick Ponomar <nick@bytefusionlabs.com>
2024-11-17 15:50:34 +0100

    Update socket.ts

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -2,13 +2,13 @@ import { io, Socket } from "socket.io-client";
 
 const isBrowser = typeof window !== "undefined";
 const host = isBrowser
-    ? (process.env.RAILWAY_PUBLIC_DOMAIN
-        ? `wss://${process.env.RAILWAY_PUBLIC_DOMAIN}`
+    ? (process.env.NEXT_PUBLIC_WEBSOCKET_DOMAIN
+        ? `wss://${process.env.NEXT_PUBLIC_WEBSOCKET_DOMAIN}`
         : "ws://localhost:3000") // Use ws:// for local, wss:// for production
     : "";
 
 console.info("isBrowser: ", isBrowser);
 console.info("Host: ", host);
-console.info("process.env.RAILWAY_PUBLIC_DOMAIN: ", process.env.RAILWAY_PUBLIC_DOMAIN);
+console.info("process.env.NEXT_PUBLIC_WEBSOCKET_DOMAIN: ", process.env.NEXT_PUBLIC_WEBSOCKET_DOMAIN);
 
 export const socketClient: Socket | null = isBrowser ? io(host, { transports: ['websocket'] }) : null;
